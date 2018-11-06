55deca0ab29615caefc8bc183a4795129e626611
hermaa57 <44037245+hermaa57@users.noreply.github.com>
2018-11-06 15:23:16 -0600

    fkakddas
    flkldsf

---
+++ team9_proj/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Biggest Loser</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<!--Insert Code Here-->
+		<!--Braden sucks at programming-->
+	
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="../js/bootstrap.js"></script>
+	</body>
+</html>
