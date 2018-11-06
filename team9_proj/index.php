fc1c7f58336bd659b332614f05d203729548de95
hermaa57 <44037245+hermaa57@users.noreply.github.com>
2018-11-06 15:28:46 -0600

    navbar bitch

--- team9_proj/index.php
+++ team9_proj/index.php
@@ -8,7 +8,23 @@
 		<link href="../css/style.css" rel="stylesheet">
 	</head>
 	<body>
-		<!--Insert Code Here-->
+		<div class="navbar navbar-inverse navbar-static-top" id="navbar">
+			<div class="container" id="navbar">
+				<a href="" class="navbar-brand">Biggest Loser</a>
+				<div class="collapse navbar-collapse navHeaderCollapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="">Home</a></li><!--Link to home page-->
+						<li class="dropdown">
+							<a href="" class="dropdown-toggle" data-toggle="dropdown">Groups <b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="">Group name</a></li><!--Link to group page-->
+								</ul>
+						</li>
+						<li><a href="">Logout</a></li><!--Link to login page-->
+					</ul>
+				</div>	
+			</div>
+		</div>
 	
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script src="../js/bootstrap.js"></script>
