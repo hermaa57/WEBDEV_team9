267047fe1040143e33d64dca23394a80e0664cae
hermaa57 <44037245+hermaa57@users.noreply.github.com>
2018-11-06 17:31:06 -0600

    navbar mobile friendly
    Oy mate the navbars pretty much done

---
+++ team9_proj/php/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>All Saints Lutheran Church</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/style.css" rel="stylesheet">
+	</head>
+	<body>
+		<!--Navbar container-->
+		<div class="navbar navbar-inverse navbar-static-top" id="navbar">
+			<div class="container" id="navbar">
+				<a href="../php/index.php" class="navbar-brand">All Saints Lutheran Church</a>
+				<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div class="collapse navbar-collapse navHeaderCollapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="active"><a  href="../php/index.php">Home</a></li>
+						<li><a href="../php/pastor.php">Pastor</a></li>
+						<li><a href="../php/about.php">About</a></li>
+						<li><a href="../php/ministries.php">Ministries</a></li>
+						<li><a href="../php/donate.php">Donate</a></li>
+						<li><a href="../php/siteMap.php">Site Map</a></li>
+						<?php
+							$login = false;
+							if(!$login){ 
+						?>
+								<li><a href="../php/login.php">Login/Sign-up</a></li>
+						<?php
+							}
+							if($login){
+						?>
+								<li><a href="../php/profile.php">Your Profile</a></li>
+								<li><a id="logout" href="../php/index.php">Logout</a></li>
+						<?php
+							}
+						?>
+					</ul>
+				</div>	
+			</div>
+		</div>
+	
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="../js/bootstrap.js"></script>
+	</body>
+</html>
+						
