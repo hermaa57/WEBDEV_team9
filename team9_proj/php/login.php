de91490b06917df33b7afc0c7bf39423cd3591e0
hermaa57 <44037245+hermaa57@users.noreply.github.com>
2018-11-07 15:36:22 -0600

    added style.css to login

--- team9_proj/php/login.php
+++ team9_proj/php/login.php
@@ -5,6 +5,7 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/style.css" rel="stylesheet">
 		<link href="../css/login.css" rel="stylesheet">
 	</head>
 	<body>
