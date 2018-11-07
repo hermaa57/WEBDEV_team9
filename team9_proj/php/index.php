a576edf689a84b3053146cbd1910e42cb5a1af82
hermaa57 <44037245+hermaa57@users.noreply.github.com>
2018-11-06 18:56:38 -0600

    Update team9_proj/php/index.php

--- team9_proj/php/index.php
+++ team9_proj/php/index.php
@@ -10,8 +10,8 @@
 	<body>
 		<!--Header container-->
 		<div class="page-header">
-			<h1>All Saints Lutheran Church</h1>
-			<img src="../images/header.jpg">
+			<h1 id="headTitle">All Saints Lutheran Church</h1>
+			<background img id="headImg" src="../images/header.jpg">
 		<!--Navbar container-->
 		<div class="navbar navbar-inverse navbar-static-top" id="navbar">
 			<div class="container" id="navbar">
