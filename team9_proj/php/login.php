1f78b206acef229e9a3bbee63dceb5d918422556
hermaa57 <44037245+hermaa57@users.noreply.github.com>
2018-11-07 15:41:30 -0600

    fixed login header

--- team9_proj/php/login.php
+++ team9_proj/php/login.php
@@ -6,7 +6,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="../css/bootstrap.min.css" rel="stylesheet">
 		<link href="../css/style.css" rel="stylesheet">
-		<link href="../css/login.css" rel="stylesheet">
 	</head>
 	<body>
 		<!--Header container-->
