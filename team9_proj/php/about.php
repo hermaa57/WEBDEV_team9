7112f0f5970afdcb9bd030a4974038e205186452
hermaa57 <44037245+hermaa57@users.noreply.github.com>
2018-11-08 17:58:03 -0600

    minor fixes

--- team9_proj/php/about.php
+++ team9_proj/php/about.php
@@ -13,7 +13,7 @@
 		<div class="header">
 			<div class="row" id="row1">
 				<div class="col-md-12">
-					<img class="img-responsive" src="../images/cross_header.jpg" id="headImg">
+					<img class="img-responsive" alt="cross" src="../images/cross_header.jpg" id="headImg">
 					<h1 id="headTitle">All Saints Lutheran Church</h1>
 				</div>
 			</div>
