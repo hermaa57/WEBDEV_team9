4c320dfd603d69f007a1d6dd3568ff6731586415
hermaa57 <44037245+hermaa57@users.noreply.github.com>
2018-11-08 17:46:33 -0600

    finished footer for now

--- team9_proj/php/donate.php
+++ team9_proj/php/donate.php
@@ -72,22 +72,22 @@
 					<div class="col-md-12 col-xs-12">
 						<ul class="list-unstyled list-inline">
 							<li class="list-inline-item">
-								<a href="https://www.facebook.com/AllSaintsOshkosh"><img id="socialImg" src="../images/facebook.png"></a>
+								<a href="https://www.facebook.com/AllSaintsOshkosh"><img id="socialImg" alt="facebook" src="../images/facebook.png"></a>
 							</li>
 							<li class="list-inline-item">
-								<a href="https://www.elca.org"><img id="socialImg" src="../images/elcaLogo.jpg"></a>
+								<a href="https://www.elca.org"><img id="socialImg" alt="ELCA" src="../images/elcaLogo.jpg"></a>
 							</li>
 							<li class="list-inline-item">
-								<a href="http://womenoftheelca.org"><img id="socialImgSmall" src="../images/elcaWomen.png"></a>
+								<a href="http://womenoftheelca.org"><img id="socialImgSmall" alt="Women of the ELCA" src="../images/elcaWomen.png"></a>
 							</li>
 							<li class="list-inline-item">
-								<a href="http://lirs.org"><img id="socialImg" src="../images/lirs.jpg"></a>
+								<a href="http://lirs.org"><img id="socialImg" alt="LIRS" src="../images/lirs.jpg"></a>
 							</li>
 							<li class="list-inline-item">
-								<a href="http://lwr.org"><img id="socialImgSmall" src="../images/lwrLogo.GIF"></a>
+								<a href="http://lwr.org"><img id="socialImgSmall" alt="LWR" src="../images/lwrLogo.GIF"></a>
 							</li>
 							<li class="list-inline-item">
-								<a href="http://www.loppw.org"><img id="socialImg" src="../images/lopp.png"></a>
+								<a href="http://www.loppw.org"><img id="socialImg" alt="LOPPW" src="../images/lopp.png"></a>
 							</li>
 						</ul>
 					</div>
@@ -95,7 +95,8 @@
 				<div class="row">
 					<div class="col-md-12 col-xs-12">
 						<p>Disclaimer: This site is under development by UW-Oshkosh students as a prototype for the organization All Saints Lutheran Church. Nothing on the site should
-						be constructed in any way as being officially connected with or representative of All Saints Lutheran Church.</p>
+						be constructed in any way as being officially connected with or representative of All Saints Lutheran Church.</p><br>
+						<p><a href="http://validator.w3.org/check/referer">Validate Me</a>
 					</div>
 				</div>
 			</div>
