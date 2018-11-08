4c3b82dd29a57148df445afa0c0d7780a884c47a
hermaa57 <44037245+hermaa57@users.noreply.github.com>
2018-11-07 20:47:43 -0600

    footer 95% done
    still needs html and css verification

--- team9_proj/php/login.php
+++ team9_proj/php/login.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>All Saints Lutheran Church</title>
 		<meta charset="UTF-8">
+		<meta name="robots" content="noindex, nofollow" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="../css/bootstrap.min.css" rel="stylesheet">
 		<link href="../css/style.css" rel="stylesheet">
@@ -125,7 +126,41 @@
 		</div>
 		
 		
-		
+		<!--Footer container-->
+		<div class="footer navbar-static-bottom">
+			<div class="container">
+				<div class="row" id="socRow">
+					<div class="col-md-12 col-xs-12">
+						<ul class="list-unstyled list-inline">
+							<li class="list-inline-item">
+								<a href="https://www.facebook.com/AllSaintsOshkosh"><img id="socialImg" src="../images/facebook.png"></a>
+							</li>
+							<li class="list-inline-item">
+								<a href="https://www.elca.org"><img id="socialImg" src="../images/elcaLogo.jpg"></a>
+							</li>
+							<li class="list-inline-item">
+								<a href="http://womenoftheelca.org"><img id="socialImgSmall" src="../images/elcaWomen.png"></a>
+							</li>
+							<li class="list-inline-item">
+								<a href="http://lirs.org"><img id="socialImg" src="../images/lirs.jpg"></a>
+							</li>
+							<li class="list-inline-item">
+								<a href="http://lwr.org"><img id="socialImgSmall" src="../images/lwrLogo.GIF"></a>
+							</li>
+							<li class="list-inline-item">
+								<a href="http://www.loppw.org"><img id="socialImg" src="../images/lopp.png"></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 col-xs-12">
+						<p>Disclaimer: This site is under development by UW-Oshkosh students as a prototype for the organization All Saints Lutheran Church. Nothing on the site should
+						be constructed in any way as being officially connected with or representative of All Saints Lutheran Church.</p>
+					</div>
+				</div>
+			</div>
+		</div>
 		
 	
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
